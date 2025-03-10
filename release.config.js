f8268fd0e0663629789d15e29e2439d7b115df69
Drew Bradford <16455249+DrewBradfordXYZ@users.noreply.github.com>
2025-03-09 23:01:06 -0400

    chore: add standard-version and .versionrc.json for version management

--- release.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  branches: [{ name: "main", prerelease: "beta" }],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    "@semantic-release/git",
-  ],
-};
