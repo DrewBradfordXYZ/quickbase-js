c4ec0ac378dd03e5d018b4ea83530e2c0fe905a2
Drew Bradford <16455249+DrewBradfordXYZ@users.noreply.github.com>
2025-03-16 15:45:37 -0400

    feat: unit tables tests pass

--- open-api/schema/tags/tables.ts
+++ open-api/schema/tags/tables.ts
@@ -27,15 +27,53 @@ export function enhanceTables(
           console.log(
             `Defining schema for ${requestName} in ${pathKey}(${method})`
           );
-          const properties = {
-            name: { type: "string" },
-          };
-          spec.definitions[requestName] = {
-            type: "object",
-            properties,
-            required: Object.keys(properties),
-            description: operation.summary || `Request body for ${opId}`,
-          };
+          let properties;
+          if (pathKey === "/tables" && method === "post") {
+            // Create Table
+            properties = {
+              name: {
+                type: "string",
+                description: "The name for the table.",
+                minLength: 1,
+              },
+              description: {
+                type: "string",
+                description:
+                  "The description for the table. If this value is not passed the default value is blank.",
+              },
+              singleRecordName: {
+                type: "string",
+                description:
+                  "The singular noun for records in the table. If this value is not passed the default value is 'Record'.",
+              },
+              pluralRecordName: {
+                type: "string",
+                description:
+                  "The plural noun for records in the table. If this value is not passed the default value is 'Records'.",
+              },
+            };
+          } else if (pathKey === "/tables/{tableId}" && method === "post") {
+            // Update Table
+            properties = {
+              name: {
+                type: "string",
+                description: "The updated name of the table.",
+              },
+              description: {
+                type: "string",
+                description: "The updated description for the table.",
+              },
+            };
+          }
+          if (properties) {
+            spec.definitions[requestName] = {
+              type: "object",
+              properties,
+              required: ["name"], // Only name is required for create
+              additionalProperties: true, // Match raw spec
+              description: operation.summary || `Request body for ${opId}`,
+            };
+          }
         }
         param.schema = { $ref: `#/definitions/${requestName}` };
         console.log(
